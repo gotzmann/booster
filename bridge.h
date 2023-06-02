ed2c2f9e82425f861cdabb1a654f7e458cf39728
Serge Gotsuliak <sgotsulyak@gmail.com>
2023-06-02 23:01:35 +0300

    bridge.h

--- bridge.h
+++ bridge.h
@@ -1,7 +1,11 @@
 #ifndef BRIDGE_H
 #define BRIDGE_H
 
+extern "C" { // ------------------------------------------------------
+
 char * status(char * jobID);
 int64_t timing(char * jobID);
+
+}  // ------------------------------------------------------
 
 #endif // BRIDGE_H
