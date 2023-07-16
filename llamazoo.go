c70ea82b217c81f28efb6faba7eddff13cb29090
Serge Gotsuliak <sgotsulyak@gmail.com>
2023-07-16 23:13:49 +0300

    v0.9.15

--- llamazoo.go
+++ llamazoo.go
@@ -67,7 +67,7 @@ import (
 	"github.com/gotzmann/llamazoo/pkg/server"
 )
 
-const VERSION = "0.9.14"
+const VERSION = "0.9.15"
 
 type Options struct {
 	Prompt        string  `long:"prompt" description:"Text prompt from user to feed the model input"`
