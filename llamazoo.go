28c79fa8cd76433a4a94320d75acd66a29104784
Serge Gotsuliak <sgotsulyak@gmail.com>
2023-09-13 20:54:46 +0300

    make

--- llamazoo.go
+++ llamazoo.go
@@ -18,14 +18,20 @@ package main
 // Perplexity for all models https://github.com/ggerganov/llama.cpp/discussions/406
 // GPTQ vs RTN Perplexity https://github.com/qwopqwop200/GPTQ-for-LLaMa
 
+// #cgo linux CFLAGS:   -I. -O3 -fPIC -pthread -std=c17
+// #cgo linux CXXFLAGS: -I. -Icommon -O3 -fPIC -pthread -std=c++17
+// #cgo linux LDFLAGS: bridge.o common.o ggml.o ggml-alloc.o llama.o k_quants.o ggml-cuda.o -lstdc++ -lm -lcublas -lculibos -lcudart -lcublasLt -lpthread -ldl -lrt -L/usr/local/cuda/lib64 -L/opt/cuda/lib64 -L/usr/local/cuda-12.2/targets/x86_64-linux/lib
+
 /*
 #include <stdlib.h>
 #include <stdint.h>
 const char * status(char * jobID);
 int64_t getPromptTokenCount(char * jobID);
-#cgo linux CFLAGS:   -I. -O3 -fPIC -pthread -std=c17
-#cgo linux CXXFLAGS: -I. -Icommon -O3 -fPIC -pthread -std=c++17
-#cgo linux LDFLAGS: bridge.o common.o ggml.o ggml-alloc.o llama.o k_quants.o ggml-cuda.o -lstdc++ -lm -lcublas -lculibos -lcudart -lcublasLt -lpthread -ldl -lrt -L/usr/local/cuda/lib64 -L/opt/cuda/lib64 -L/usr/local/cuda-12.2/targets/x86_64-linux/lib
+#cgo linux MK_CPPFLAGS = -I. -Icommon -DNDEBUG -DLOG_DISABLE_LOGS -DGGML_USE_K_QUANTS -DGGML_USE_CUBLAS -I/usr/local/cuda/include -I/opt/cuda/include -I$(CUDA_PATH)/targets/x86_64-linux/include
+#cgo linux MK_CFLAGS   = -O3 -std=c17   -fPIC -D_GNU_SOURCE -march=native -mtune=native
+#cgo linux MK_CXXFLAGS = -O3 -std=c++17 -fPIC -D_GNU_SOURCE -march=native -mtune=native
+#cgo linux MK_LDFLAGS  = bridge.o common.o ggml.o ggml-alloc.o llama.o k_quants.o ggml-cuda.o -lstdc++ -lm -lcublas -lculibos -lcudart -lcublasLt -lpthread -ldl -lrt -L/usr/local/cuda/lib64 -L/opt/cuda/lib64 -L$(CUDA_PATH)/targets/x86_64-linux/lib
+#cgo linux NVCCFLAGS = --forward-unknown-to-host-compiler -use_fast_math -arch=native -DGGML_CUDA_DMMV_X=32 -DGGML_CUDA_MMV_Y=1 -DK_QUANTS_PER_ITERATION=2
 #cgo darwin CFLAGS:   -I. -O3 -fPIC -pthread -std=c17 -DNDEBUG -DGGML_USE_METAL -DGGML_METAL_NDEBUG -mcpu=native
 #cgo darwin CXXFLAGS: -I. -Icommon -O3 -fPIC -pthread -std=c++17 -DNDEBUG -DGGML_USE_METAL -mcpu=native
 #cgo darwin LDFLAGS: bridge.o common.o ggml.o ggml-alloc.o llama.o k_quants.o ggml-metal.o -lstdc++ -framework Accelerate -framework Foundation -framework Metal -framework MetalKit -framework MetalPerformanceShaders
