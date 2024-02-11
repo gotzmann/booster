8a8cc434f6c2ef91279940be4a9dbaaa7be30496
Serge Gotsuliak <sgotsulyak@gmail.com>
2024-02-11 14:51:01 +0300

    ..

--- collider.go
+++ collider.go
@@ -22,7 +22,7 @@ int64_t getPromptTokenCount(char * jobID);
 #cgo darwin CFLAGS:   -O3 -std=c17   -I.          -fPIC -pthread -mcpu=native                -DNDEBUG -DGGML_USE_K_QUANTS -D_XOPEN_SOURCE=600 -D_DARWIN_C_SOURCE -DGGML_USE_METAL  -DGGML_METAL_NDEBUG -DACCELERATE_NEW_LAPACK -DACCELERATE_LAPACK_ILP64
 #cgo linux  CXXFLAGS: -O3 -std=c++17 -I. -Icommon -fPIC -pthread -march=native -mtune=native -DNDEBUG -DGGML_USE_K_QUANTS -D_XOPEN_SOURCE=600 -D_GNU_SOURCE      -DGGML_USE_CUBLAS -DLOG_DISABLE_LOGS -I/usr/local/cuda/include -I/opt/cuda/include -I/usr/local/cuda/targets/x86_64-linux/include
 #cgo darwin CXXFLAGS: -O3 -std=c++17 -I. -Icommon -fPIC -pthread -mcpu=native                -DNDEBUG -DGGML_USE_K_QUANTS -D_XOPEN_SOURCE=600 -D_DARWIN_C_SOURCE -DGGML_USE_METAL  -DGGML_METAL_NDEBUG -DACCELERATE_NEW_LAPACK -DACCELERATE_LAPACK_ILP64
-#cgo linux  LDFLAGS:  cpp/llama.o cpp/bridge.o cpp/janus.o cpp/ggml.o cpp/ggml-backend.o cpp/ggml-alloc.o cpp/ggml_quants.o cpp/ggml-cuda.o  -lstdc++ -lm -lcublas -lculibos -lcudart -lcublasLt -lpthread -ldl -lrt -L/usr/local/cuda/lib64 -L/opt/cuda/lib64 -L/usr/local/cuda/targets/x86_64-linux/lib
+#cgo linux  LDFLAGS:  cpp/llama.o cpp/bridge.o cpp/janus.o cpp/ggml.o cpp/ggml-backend.o cpp/ggml-alloc.o cpp/ggml-quants.o cpp/ggml-cuda.o  -lstdc++ -lm -lcublas -lculibos -lcudart -lcublasLt -lpthread -ldl -lrt -L/usr/local/cuda/lib64 -L/opt/cuda/lib64 -L/usr/local/cuda/targets/x86_64-linux/lib
 #cgo darwin LDFLAGS:  cpp/llama.o cpp/bridge.o cpp/janus.o cpp/ggml.o cpp/ggml-backend.o cpp/ggml-alloc.o cpp/ggml-quants.o cpp/ggml-metal.o -lstdc++ -framework Accelerate -framework Foundation -framework Metal -framework MetalKit
 */
 import "C"
